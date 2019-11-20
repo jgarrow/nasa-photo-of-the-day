3b6454bbb6bcf2bed2541d56037c1edec6add6a3
Janessa Garrow <janessa.garrow@gmail.com>
2019-11-19 21:08:48 -0700

    Added twinkling star background

--- src/App.js
+++ src/App.js
@@ -6,31 +6,41 @@ import Apod from "./components/apod";
 import Pagination from "./components/pagination";
 
 function App() {
-  const [currentDate, setCurrentDate] = useState(new Date());
-  const [apodData, setApodData] = useState({});
-  const [apodDate, setApodDate] = useState("");
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [apodData, setApodData] = useState({});
+    const [apodDate, setApodDate] = useState("");
 
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.nasa.gov/planetary/apod?api_key=ITUHEmrHUbMkITyMyrkzMglZPFMp9b3c1vVX2LAJ${apodDate}`
-      )
-      .then(response => {
-        setApodData(response.data);
-      })
-      .catch(error => console.log(`There was an error: ${error}`));
-  }, [apodDate]);
+    useEffect(() => {
+        axios
+            .get(
+                `https://api.nasa.gov/planetary/apod?api_key=ITUHEmrHUbMkITyMyrkzMglZPFMp9b3c1vVX2LAJ${apodDate}`
+            )
+            .then(response => {
+                setApodData(response.data);
+            })
+            .catch(error => console.log(`There was an error: ${error}`));
+    }, [apodDate]);
 
-  return (
-    <div className="App">
-      {apodData === {} ? <h3>Loading...</h3> : <Apod data={apodData} />}
-      <Pagination 
-        currentDate={currentDate}
-        setApodDate={setApodDate}
-        setCurrentDate={setCurrentDate}
-      />
-    </div>
-  );
+    return (
+        <>
+            <div className="atmosphere-container">
+                <div class="atmosphere"></div>
+                <div class="stars"></div>
+            </div>
+            <div className="App">
+                {apodData === {} ? (
+                    <h3>Loading...</h3>
+                ) : (
+                    <Apod data={apodData} />
+                )}
+                <Pagination
+                    currentDate={currentDate}
+                    setApodDate={setApodDate}
+                    setCurrentDate={setCurrentDate}
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
